4b14fb2c7b801a781692c4c1c14a367631fe325c
asr <denocris>
2019-02-06 17:34:46 +0100

    dnn scripts added

---
+++ dnn_train/script/launch.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+
+# Usage:
+# $: bash launch.sh <model-name> <checkpoints-path>
+# Ex: bash launch.sh test-sei /home/asr/prj_SlotTagger/dnn_train/checkpoints/
+
+
+# The name of the ambiguity must be its folder name
+AMBIGUITY=è_e
+
+mkdir -p ../checkpoints
+
+echo $'\n-----------------------------------------------------\n'
+echo $'                  AMBIGUITY: '$AMBIGUITY
+echo $'\n-----------------------------------------------------\n'
+
+MODEL_NAME=$1
+CHECKPOINTS_PATH=$2
+
+ROOT='/home/asr/prj_SlotTagger/dnn_train/'
+TRAIN_DATA_PATH=$ROOT'datasets/'$AMBIGUITY'/train_data_v5.tsv'
+VALID_DATA_PATH=$ROOT'datasets/'$AMBIGUITY'/valid_data_v5.tsv'
+VOCAB_PATH=$ROOT'datasets/'$AMBIGUITY'/vocab_10k_swin_v4.tsv'
+NUM_WORDS_PATH=$ROOT'datasets/'$AMBIGUITY'/n_words_10k_swin_v4.tsv'
+TRAINING_SIZE=$(cat $TRAIN_DATA_PATH | wc -l )
+
+echo $'\n-----------------------------------------------------\n'
+echo $'                  TRAINING STARTING                    \n'
+echo 'Taraining datset: ' $TRAIN_DATA_PATH
+echo $'                                                       \n'
+echo 'Taraining size: ' $TRAINING_SIZE
+
+# for num in '1 2 4 8 16 32';
+# do
+# for num in 1 2 4 8 16 32;
+# do
+# for lrate in 0.000001 0.0000001;
+# do           #0.000005
+#LRATE=$(($num * 8))
+python3 main.py ${MODEL_NAME}_$lrate ${CHECKPOINTS_PATH} ${TRAIN_DATA_PATH} ${VALID_DATA_PATH} ${VOCAB_PATH} ${NUM_WORDS_PATH} $TRAINING_SIZE #$lrate
+#done
+
+echo $'\n-----------------------------------------------------\n'
+echo $'                  TRAINING FINISHED                    \n'
+
+echo 'Model saved in: ' $CHECKPOINTS_PATH$MODEL_NAME
+echo $'                                                       '
+echo $'\n-----------------------------------------------------\n'
